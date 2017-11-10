a09f99929c676f7d72bb42afe4c48848cfb8b62d
Ulf <ulfbiallas@googlemail.com>
2017-11-10 12:31:55 +0000

    add form and list to  waitlist component

--- src/javascript/views/waitlist.js
+++ src/javascript/views/waitlist.js
@@ -3,12 +3,37 @@ import Slider from 'material-ui/Slider';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+import TextField from 'material-ui/TextField';
 
 export default class WaitList extends React.Component {
   render() {
     return (
       <div style={{width: '400px'}}>
-      the actual list...
+
+      <TextField hintText="Interests"/>
+      <TextField hintText="Name"/>
+
+      <List>
+            <ListItem
+              primaryText="Brendan Lim"
+              leftAvatar={<Avatar src="images/ok-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+            <ListItem
+              primaryText="Eric Hoffman"
+              leftAvatar={<Avatar src="images/kolage-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+            <ListItem
+              primaryText="Grace Ng"
+              leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+        </List>
+
       </div>
     );
   }
