416126d49162a7ce65fb99869cbd2ae628010f3e
Ulf <ulfbiallas@googlemail.com>
2017-11-10 16:06:45 +0000

    prepare signup page for user creation

--- src/javascript/views/signup.js
+++ src/javascript/views/signup.js
@@ -6,16 +6,52 @@ import RaisedButton from 'material-ui/RaisedButton';
 import {NavLink} from 'react-router-dom'
 
 export default class Signup extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      airport: 'MUC',
+      waitingTime: 30
+    };
+    this.changeAirport = this.changeAirport.bind(this);
+    this.changeWaitingTime = this.changeWaitingTime.bind(this);
+    this.saveAndContinue = this.saveAndContinue.bind(this);
+  }
+
+  changeAirport(event, key, value) {
+    this.setState({
+      airport: value
+    });
+  }
+
+  changeWaitingTime(event, value) {
+    this.setState({
+      waitingTime: Math.floor(value)
+    });
+  }
+
+  saveAndContinue() {
+    console.log("Create user with: " + JSON.stringify(this.state));
+    this.props.history.push("/waitlist");
+  }
+
   render() {
+    const {waitingTime} = this.state;
     return (
       <div style={{width: '400px'}}>
-        <DropDownMenu value={1} onChange={this.handleChange}>
-          <MenuItem value={1} primaryText="Munich" />
-          <MenuItem value={2} primaryText="Geneva" />
-          <MenuItem value={3} primaryText="Copenhagen" />
+        <DropDownMenu value={"MUC"} onChange={this.changeAirport}>
+          <MenuItem value={"MUC"} primaryText="Munich" />
+          <MenuItem value={"GVA"} primaryText="Geneva" />
+          <MenuItem value={"CPH"} primaryText="Copenhagen" />
         </DropDownMenu>
-        <Slider defaultValue={0.5} />
-        <NavLink to="/waitlist"><RaisedButton label='See who is waiting'/></NavLink>
+        <Slider
+          min={15}
+          max={300}
+          defaultValue={30}
+          onChange={this.changeWaitingTime}
+        />
+        <div>Waiting time: {waitingTime} minutes.</div>
+        <RaisedButton label='See who is waiting' onClick={this.saveAndContinue}/>
       </div>
     );
   }
