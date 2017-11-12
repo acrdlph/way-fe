0689cf5161b7fbbc03423e3811238ba828296ba8
Vimukthi <vbwickramasinghe@gmail.com>
2017-11-12 23:36:20 +0100

    added correct proxy config for dev

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+DOCKER_LOGIN="$(aws ecr get-login --no-include-email --region eu-central-1)"
+
+$DOCKER_LOGIN
+
 echo "Building static content"
 
 npm run build
+
+cp -R src/static/assets build/assets
 
 echo "Building docker image"
 
@@ -10,8 +16,10 @@ docker build -t waitlist-fe .
 
 echo "Tagging image"
 
-docker tag waitlist-fe:latest 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:latest
+CURRENT_HASH="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse HEAD | cut -c1-6)"
+
+docker tag waitlist-fe 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
 
 echo "Pushing image to repo"
 
-docker push 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:latest
+docker push 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
