8c974686f640f60a852d9048cd46d14911c89dcc
ulfbiallas <ulfbiallas@googlemail.com>
2017-11-15 22:53:11 +0100

    full page height background color

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './index.less';
 
 ReactDOM.render((
-  <div>
+  <div className='full-height'>
     <div className='col-md-3 col-sm-2'/>
-    <div className='col-md-6 col-sm-8 col-xs-12'>
+    <div className='full-height main col-md-6 col-sm-8 col-xs-12'>
       <App/>
     </div>
     <div className='col-md-3 col-sm-2'/>
