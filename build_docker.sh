f7d2dd61641bf392af23e131ea27aa3c7ded8217
Vimukthi <vbwickramasinghe@gmail.com>
2017-11-17 21:28:08 +0100

    update build script to only build master

--- build_docker.sh
+++ build_docker.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
-DOCKER_LOGIN="$(aws ecr get-login --no-include-email --region eu-central-1)"
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$BRANCH" = "master" ]; then
+    DOCKER_LOGIN="$(aws ecr get-login --no-include-email --region eu-central-1)"
 
-$DOCKER_LOGIN
+    $DOCKER_LOGIN
 
-echo "Building static content"
+    echo "Building static content"
 
-npm install
+    npm install
 
-./node_modules/.bin/gulp build --hostname ecs-eu-dev-1571006243.eu-central-1.elb.amazonaws.com --port 8080
+    ./node_modules/.bin/gulp build --hostname ecs-eu-dev-1571006243.eu-central-1.elb.amazonaws.com --port 8080
 
-echo "Building docker image"
+    echo "Building docker image"
 
-docker build -t waitlist-fe .
+    docker build -t waitlist-fe .
 
-echo "Tagging image"
+    echo "Tagging image"
 
-CURRENT_HASH="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse HEAD | cut -c1-6)"
+    CURRENT_HASH="$BRANCH-$(git rev-parse HEAD | cut -c1-6)"
 
-docker tag waitlist-fe 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
+    docker tag waitlist-fe 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
 
-echo "Pushing image to repo"
+    echo "Pushing image to repo"
 
-docker push 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
+    docker push 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
 
-echo "Deploying to eu-dev"
+    echo "Deploying to eu-dev"
 
-./ecs-deploy-0ab06b -r eu-central-1 -c eu-dev -n wailist-fe-dev -i 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
+    echo "Deploying to AWS";
+    ./ecs-deploy-0ab06b -r eu-central-1 -c eu-dev -n wailist-fe-dev -i 614992511822.dkr.ecr.eu-central-1.amazonaws.com/waitlist-fe:$CURRENT_HASH
+else
+    echo "Not deploying to AWS";
+fi
