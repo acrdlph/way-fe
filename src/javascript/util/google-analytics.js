c72566a4e8f86742083211b4e484f8fe0c8fa37a
ulfbiallas <ulfbiallas@googlemail.com>
2017-11-30 08:34:26 +0100

    add certain events that can be tracked

--- src/javascript/util/google-analytics.js
+++ src/javascript/util/google-analytics.js
@@ -1,5 +1,13 @@
 import ReactGA from 'react-ga';
 
+export const actions = {
+  USER_SELECTED_LOCATION: 'User selected a location',
+  USER_CHANGED_WAITING_TIME: 'User changed waiting time',
+  USER_UPLOADED_PROFILE_PHOTO: 'User uploaded profile photo',
+  USER_FILLED_OUT_PROFILE_FORM: 'User filled out profile form',
+  USER_SEND_MESSAGE: 'User send chat message'
+};
+
 export const initializeGoogleAnalytics = () => {
   if(GOOGLE_ANALYTICS_ID.length > 0) {
     ReactGA.initialize(GOOGLE_ANALYTICS_ID, {
@@ -11,5 +19,15 @@ export const initializeGoogleAnalytics = () => {
 export const trackPageView = (path) => {
   if(GOOGLE_ANALYTICS_ID.length > 0) {
     ReactGA.pageview(path);
+  }
+};
+
+export const trackEvent = (action, value) => {
+  if(GOOGLE_ANALYTICS_ID.length > 0) {
+    ReactGA.event({
+      category: 'User',
+      action,
+      value
+    });
   }
 };
