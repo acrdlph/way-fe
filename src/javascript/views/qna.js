d33df9f13a64238ea68a6bcb186803f587fa68fb
obasilakis <vasileios.georgopoulos@gmail.com>
2018-09-03 11:44:00 +0200

    adding qna component

---
+++ src/javascript/views/qna.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class QnA extends React.Component {
+  render() {
+    return <div> yop </div>;
+  }
+}
+
+export default connect()(QnA);
