435f239151f2f40e2b06f5e1a129cfacb7f11056
Flukal <fluviobass@gmail.com>
2018-11-07 11:03:28 +0100

    Deleted Bootstrap component

--- src/javascript/views/qna.js
+++ src/javascript/views/qna.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import {
-  Button, Form, FormGroup, Input,
-} from 'reactstrap';
 import { TextField } from '@material-ui/core';
-
 
 import {
   postQuestion,
