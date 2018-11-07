eec4128ec7636479c01f2d25e51747459f16f5eb
Flukal <fluviobass@gmail.com>
2018-11-07 15:07:26 +0100

    NoChat component created, design and responsiveness done

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -16,6 +16,7 @@ import Chat from './views/chat';
 import InteractionConfirmation from './views/interaction-confirmation';
 import Feedback from './views/feedback';
 import LegalNotice from './views/legal-notice';
+import NoChat from './components/noChat'
 import './app.less';
 
 class App extends React.Component {
@@ -84,6 +85,7 @@ class App extends React.Component {
           <Route exact path="/waitlist/" component={WaitList} />
           <Route exact path="/waitlist/:locationId" component={WaitList} />
           <Route exact path="/chat/:chatPartnerId" component={Chat} />
+          <Route exact path="/chat" component={NoChat}/>
           <Route
             exact
             path="/confirm-interaction/:interactionCode"
